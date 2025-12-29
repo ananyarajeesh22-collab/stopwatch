4b33f31363d67ceb8c29049b9061199209a42dc5
ananyarajeesh22-collab <ananyarajeesh22@gmail.com>
2025-12-29 15:51:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const display = document.querySelector('.display');
+const startStopBtn = document.getElementById('startStopBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+let startTime;
+let updatedTime;
+let difference;
+let tInterval;
+let running = false;
+let lapCounter = 0;
+
+function startStop() {
+    if (!running) {
+        startTime = new Date().getTime();
+        tInterval = setInterval(getShowTime, 1);
+        running = true;
+        startStopBtn.innerHTML = "Stop";
+    } else {
+        clearInterval(tInterval);
+        running = false;
+        startStopBtn.innerHTML = "Start";
+    }
+}
+
+function reset() {
+    clearInterval(tInterval);
+    running = false;
+    startStopBtn.innerHTML = "Start";
+    display.innerHTML = "00:00:00";
+}
+
+function getShowTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    display.innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+
+startStopBtn.addEventListener('click', startStop);
+resetBtn.addEventListener('click', reset);
